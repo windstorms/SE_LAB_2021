aef5935c88fa9b50f1f1e4b40aa1f050d994bbd4
windstorms <475605195@qq.com>
2021-10-06 22:51:10 +0800

    1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner scanner =  new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        System.out.println(a+b);
+    }
+}
